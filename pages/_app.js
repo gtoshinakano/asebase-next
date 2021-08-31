add0aa9939e455ab783017156a6ccafcc20c153d
Toshi Nakano <gtoshinakano@gmail.com>
2021-08-31 03:23:28 -0300

    Added remixicons CDN

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,23 @@
+import Head from 'next/head'
 import { Provider } from 'next-auth/client';
 import '../styles/tailwind.css';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider
-      options={{
-        clientMaxAge: 0,
-        keepAlive: 0,
-      }}
-      session={pageProps.session}
-    >
-      <Component {...pageProps} />
-    </Provider>
+    <>
+      <Head>
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet" />
+      </Head>
+      <Provider
+        options={{
+          clientMaxAge: 0,
+          keepAlive: 0,
+        }}
+        session={pageProps.session}
+      >
+        <Component {...pageProps} />
+      </Provider>
+    </>
   );
 }
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,15 +1,22 @@
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/client';
+import useTranslation from 'next-translate/useTranslation'
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
 // rendering, and avoids any flash incorrect content on initial page load.
 export default function Header() {
   const [session, loading] = useSession();
+  const { t, lang } = useTranslation('common')
 
   return (
-    <header>
-      <nav className="text-red-500 text-sm">Use in Next.js Version 3.*</nav>
+    <header className="w-full p-3 bg-rose-500 flex">
+      <nav className="text-white text-sm">
+        <h4 className="flex-grow">
+          <i class="ri-earth-fill"></i>{
+          session ? t("greeting-msg", session.user.name) : t("greeting-msg", {name: "visitante"})}</h4>
+        
+      </nav>
     </header>
   );
 }
