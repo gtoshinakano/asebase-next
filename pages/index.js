50c6846f6949eb8b004b18cb7f2de6763e4451bd
Toshi Nakano <gtoshinakano@gmail.com>
2021-08-31 01:06:46 -0300

    structured src folder

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,43 @@
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/client';
 import Layout from '../components/layout';
-import useTranslation from 'next-translate/useTranslation';
+import AccessDenied from '../components/access-denied';
 
 export default function Page() {
-  const { t } = useTranslation('common');
-  const example = t('variable-example', { count: 42 });
+  const [session, loading] = useSession();
+  const [content, setContent] = useState();
+
+  // Fetch content from protected route
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('/api/examples/protected');
+      const json = await res.json();
+      if (json.content) {
+        setContent(json.content);
+      }
+    };
+    fetchData();
+  }, [session]);
+
+  // When rendering client side don't display anything until loading is complete
+  if (typeof window !== 'undefined' && loading) return null;
+
+  // If no session exists, display access denied message
+  if (!session) {
+    return (
+      <Layout>
+        <AccessDenied />
+      </Layout>
+    );
+  }
+
+  // If session exists, display content
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p className="text-sky-500">
-        This is an example site to demonstrate how to use{' '}
-        <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+      <h1>Protected Page</h1>
+      <p>
+        <strong>{content || '\u00a0'}</strong>
       </p>
-      <p>{example}</p>
     </Layout>
   );
 }
