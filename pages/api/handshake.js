92adf3822e075788046d86f9c7627e159f05fd80
Gabriel Toshinori Nakano <gtoshinakano@gmail.com>
2021-09-22 13:59:42 -0300

    Altered handshake

--- pages/api/handshake.js
+++ pages/api/handshake.js
@@ -1,11 +1,13 @@
 // This is an example of how to read a JSON Web Token from an API route
 import jwt from 'next-auth/jwt';
+import { getSession } from "next-auth/client"
 import { query, insertId } from '@lib/db'
 
 const secret = process.env.SECRET;
 
 const get = async (req, res) => {
   const token = await jwt.getToken({ req, secret });
+  const session = await getSession({ req });
 
   if(!token) res.status(400).json({message: "I refuse to handshake with you"})
   else{
@@ -18,7 +20,10 @@ const get = async (req, res) => {
       const userInfo = user_info[0]
       console.log(userInfo)
       if(userInfo.blocked) res.status(200).json({message: "You are blocked for transgress our Terms of Use", action: "force-sign-out-user" })
-      else res.status(200).json({message: "High Five 🖐", uid: userInfo.id, data: userInfo })
+      else res.status(200).json({
+        message: "High Five 🖐", 
+        uid: userInfo.id, 
+        data: {...userInfo, ...session.user} })
     }
     
   }
