aadd948eb7df6a5d4b15170f5e5fbb18728a3b9f
Toshi Nakano <gtoshinakano@gmail.com>
2022-04-20 16:46:39 +0900

    Demo video

--- src/Components/Views/AccessDenied/index.js
+++ src/Components/Views/AccessDenied/index.js
@@ -2,16 +2,21 @@
 
 export default function AccessDenied() {
   return (
-    <>
-      <h1>Access Denied</h1>
-      <p>
+    <div className="flex flex-col pt-72 overflow-hidden justify-center">
+      <h1 className="mx-auto mb-2 text-2xl font-bold text-center">
+        Welcome to this Demo!
+      </h1>
+      <span className="mx-auto block text-sm mb-8 text-center font-semibold">DB of Brazilian Ex Trainees and Students <br />from Japanese Internship Programs</span>
+      <span className="mx-auto block text-sm">This project uses :</span>
+      <h1 className="mx-auto text-blue-500 text-xs font-semibold">NextAuth, React Query, Tailwind and Styled Components and others</h1>
+      <p className="mx-auto text-xs">
         {/* <a href="/api/auth/signin"
            onClick={(e) => {
            e.preventDefault()
            signIn()
         }}></a> */}
-        You must be signed in to view this page
+        This project will be finished one day. All support is welcome.
       </p>
-    </>
+    </div>
   );
 }
