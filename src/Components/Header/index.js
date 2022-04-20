4535f175facd9b4c48693c1cc2791383bca27e01
Toshi Nakano <gtoshinakano@gmail.com>
2022-04-20 15:12:02 +0900

    Handshake SQL insert fix

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -2,6 +2,7 @@ import Link from '@Components/Link';
 import { signIn, signOut, useSession } from 'next-auth/client';
 import useTranslation from 'next-translate/useTranslation';
 import { Button } from '@Styled/Button';
+import { useRouter } from 'next/router';
 
 // The approach used in this component shows how to build a sign in and sign out
 // component that works on pages which support both client and server side
@@ -9,6 +10,7 @@ import { Button } from '@Styled/Button';
 export default function Header() {
   const [session, loading] = useSession();
   const { t, lang } = useTranslation('common');
+  const router = useRouter()
 
   return (
     <header className="w-full p-3 bg-rose-500 fixed">
@@ -23,14 +25,16 @@ export default function Header() {
             ? t('greeting-msg', { name: session.user.email })
             : t('greeting-msg', { name: 'visitante' })}
         </h4>
-        {session && <Link href="member/dashboard">
-          <Button >
-          <i className="ri-logout-box-r-line mr-24"></i>Profile 
+        {session && 
+          <Button 
+            onClick={() => router.push("member/dashboard")}
+            className="mr-4 bg-white font-semibold text-black">
+          <i className="ri-file-user-line mr-2 text-sky-400"></i>Profile 
           </Button>
-        </Link>
+      
         }
         {session ? (
-          <Button onClick={() => signOut()}>
+          <Button className="bg-gray-200" onClick={() => signOut()}>
             <i className="ri-logout-box-r-line mr-2"></i>Sair
           </Button>
         ) : (
