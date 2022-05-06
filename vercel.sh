d0a6e695ea82a5069e4320cff0e4c27cb967ccd0
Toshi Nakano <gtoshinakano@gmail.com>
2022-05-07 08:30:55 +0900

    Vercel Tuto sh copy

--- vercel.sh
+++ vercel.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-# Vercel deployment "Ignored Build Step" to prevent building
-# https://vercel.com/docs/concepts/projects/overview#ignored-build-step
-
-echo "VERCEL_ENV: $VERCEL_ENV"
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_ENV" == "production" || "$VERCEL_GIT_COMMIT_REF" == "development"]] then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" == "development"  ]] ; then
   # Proceed with the build
-  echo "✅ - Build Ok"
+    echo "✅ - Build Ok"
   exit 1;
 
 else
